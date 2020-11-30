fd94cc48815f02da83b8f783effcaccab06ace78
shengxin321 <47261741+shengxin321@users.noreply.github.com>
2020-11-29 23:34:49 -0500

    Update calTPM.R

--- RNA-seq data analysis/calTPM.R
+++ RNA-seq data analysis/calTPM.R
@@ -1,6 +1,6 @@
 # Here we used read counts (output of RSEM) and effective gene length (output of RSEM) to calculate the TPM
 
-#load RNA-seq and their matching phenotype data
+#Load RNA-seq and their matching phenotype data
 load("$FILENAME.RData")
 library(Biobase)
 library(GenomicFeatures)
@@ -20,6 +20,27 @@ $PHENOTYPE_MATRIX[$PHENOTYPE_MATRIX$Pairend==0,]->$PHENOTYPE_MATRIX_SE
 $READ_COUNTS_MATRIX[,rownames($PHENOTYPE_MATRIX_PE)]->$READ_COUNTS_MATRIX_PE
 $READ_COUNTS_MATRIX[,rownames($PHENOTYPE_MATRIX_SE)]->$READ_COUNTS_MATRIX_SE
 
+#Load estimated effective gene length (output of RSEM)
+read.table("$EFFECTIVE_GENE_LEN_FILENAME.txt",header=F,row.name=1)->$EFFECTIVE_GENE_LEN
+zero<-$EFFECTIVE_GENE_LEN==0
+$EFFECTIVE_GENE_LEN[zero,]<-1
+                                        
+ #From read counts to TPM
+$READ_COUNTS_DGE_SE<-DGEList(counts=$READ_COUNTS_MATRIX_SE,group=$PHENOTYPE_MATRIX_SE$GFR)
+$READ_COUNTS_DGE_SE<-calcNormFactors($READ_COUNTS_DGE_SE)
+$PHENOTYPE_MATRIX_SE.design<-model.matrix(~$PHENOTYPE_MATRIX_SE$GFR)
+$READ_COUNTS_DGE_SE<-estimateDisp($READ_COUNTS_DGE_SE,$PHENOTYPE_MATRIX_SE.design)
+$READ_COUNTS_DGE_SE<-estimateCommonDisp($READ_COUNTS_DGE_SE)
+rpkm($READ_COUNTS_DGE_SE)->$READ_COUNTS_DGE_SE
+apply($READ_COUNTS_DGE_SE,2,sum)->$TOTAL_LIB
+$TPM_MATRIX=t((t($READ_COUNTS_DGE_SE)/$TOTAL_LIB)*(10^6))
+
+                                        
+                                        
+                                       
+                                        
+                                        
+                                        
 
 
 
