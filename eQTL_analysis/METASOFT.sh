b70b40754a52462d529627740a7333a988a4071e
shengxin321 <47261741+shengxin321@users.noreply.github.com>
2020-11-30 12:45:59 -0500

    Update METASOFT.sh

--- eQTL_analysis/METASOFT.sh
+++ eQTL_analysis/METASOFT.sh
@@ -1,4 +1,4 @@
 java -jar Metasoft.jar -input $FILENAME.txt -mvalue -mvalue_method mcmc -pvalue_table HanEskinPvalueTable.txt -output $OUTPUT
 
 #P-M plot
-ython pmplot.py $INPUT.txt $OUTPUT.txt title.txt studyorder.txt $SNP $GENE $OUTPUT.pdf 
+python pmplot.py $INPUT.txt $OUTPUT.txt title.txt studyorder.txt $SNP $GENE $OUTPUT.pdf 
