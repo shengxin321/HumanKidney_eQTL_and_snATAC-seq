67c5e369ab929ee94af1504f64328591231d286b
shengxin321 <47261741+shengxin321@users.noreply.github.com>
2020-11-30 11:58:22 -0500

    Update Imputation.sh

--- Genotype_data_analysis/Imputation.sh
+++ Genotype_data_analysis/Imputation.sh
@@ -1,1 +1,6 @@
+#Prephasing, $i represents for chromosome ID, $j represents for each chunk
+shapeit -B chr$i.filtered -M genetic_map_chr$i_combined_b37.txt --exclude-snp chr$i.alignments.snp.strand.exclude -O chr$i_noref.phased --thread 1
+
+#Impute
+impute2 -use_prephased_g -known_haps_g chr$i_noref.phased.haps -h 1000GP_Phase3_chr$i.hap.gz -l  1000GP_Phase3_chr$i.legend.gz -m genetic_map_chr$i_combined_b37.txt -int 0 5000000 -Ne 20000 -o chr$i_chunk$j.imputed
 
